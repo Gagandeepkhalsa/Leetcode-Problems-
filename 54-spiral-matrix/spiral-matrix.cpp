c3d1859515afbd838eb85f3d9a11ec432f4e1637
Gagandeepkhalsa <119283324+Gagandeepkhalsa@users.noreply.github.com>
2025-06-18 11:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (79.48%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -12,7 +12,6 @@ class Solution {
     int r2 = m - 1;
     int c2 = n - 1;
 
-    // Repeatedly add matrix[r1..r2][c1..c2] to `ans`.
     while (ans.size() < m * n) {
       for (int j = c1; j <= c2 && ans.size() < m * n; ++j)
         ans.push_back(matrix[r1][j]);
